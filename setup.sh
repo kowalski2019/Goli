aa8e29174231b0a7467b560cb4103a185bf6d2e7
Claude Stephan M. Kouame <claudestephane2010@live.fr>
2025-11-08 08:10:19 +0100

    Improve setup/installation script.

--- setup.sh
+++ setup.sh
@@ -1,30 +1,40 @@
 #!/bin/bash
-curr_dir=`pwd`
 
-function source_func() {
+curr_dir=`pwd`
+goli_work_dir="/usr/local/sbin/goli"
+go_download_link="https://go.dev/dl/go1.20.3.linux-amd64.tar.gz"
+go_installer="go1.20.3.linux-amd64.tar.gz"
+
+source_func() {
     source "/etc/profile"
 }
 
-function exit_func() {
+exit_func() {
     exit 1
 }
 
-function install() {
-    echo "Installing Goli ..."
-    # 64 | openssl sha256 | awk '{ print $2 }'
+apply_container_registry_settings() {
+    # https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens
+    # https://github.com/settings/tokens/new
+    # https://docs.github.com/en/packages/working-with-a-github-packages-registry/working-with-the-container-registry
 
-    default_authkey=`openssl rand -base64 64 | openssl sha256 | awk '{ print $2 }'`
+    read -p "Enter your github username" GH_USERNAME 
+    read -sp "Enter your github Personal Access Token" GH_ACCESS_TOKEN
+    export CR_PAT=$GH_ACCESS_TOKEN
+    echo $CR_PAT | docker login ghcr.io -u $GH_USERNAME --password-stdin
 
-    read -p "Enter an authorization key (default '${default_authkey}' ): " AUTHKEY
+    echo "Authenticating to the Container registry successfully done"
+}
 
-    [ -z "$AUTHKEY" ] && auth_key="${default_authkey}" || auth_key="$AUTHKEY"
+function setup_apache_reverse_proxy() {
+    echo "Apache reverse proxy is available"
+}
 
-    # check if go is Intalled
-    echo ""
+function setup_nginx_reverse_proxy() {
+    echo "Nginx reverse proxy is available"
+}
 
-    go_download_link="https://go.dev/dl/go1.20.3.linux-amd64.tar.gz"
-    go_installer="go1.20.3.linux-amd64.tar.gz"
-
+install_go(){
     which go
     if [ $? -ne 0 ]; then 
         echo "Go does not exists!"
@@ -54,17 +64,31 @@ function install() {
     else
         echo "Go is already installed"
     fi
+}
 
-   
+compile_and_install_binaries() {
+    ## Compile and Install go binary
+    cd "${curr_dir}/goli" && /usr/local/go/bin/go get && /usr/local/go/bin/go build -o "${goli_work_dir}/goli" main.go && cd -
+}
 
-    goli_work_dir="/usr/local/sbin/goli"
+install() {
+    echo "Installing Goli ..."
     mkdir -p /usr/local/sbin/goli
 
+    # 64 | openssl sha256 | awk '{ print $2 }'
+
+    default_authkey=`openssl rand -base64 64 | openssl sha256 | awk '{ print $2 }'`
+
+    read -p "Enter an authorization key (default '${default_authkey}' ): " AUTHKEY
+
+    [ -z "$AUTHKEY" ] && auth_key="${default_authkey}" || auth_key="$AUTHKEY"
+
+    # check if go is Intalled
+    install_go
+
+    compile_and_install_binaries
+
     ## Change port possibility
-
-
-    ## Compile and Install go binary
-    cd "${curr_dir}/goli" && /usr/local/go/bin/go get && /usr/local/go/bin/go build -o "${goli_work_dir}/goli" main.go && cd ..
 
     ## Create Goli Toml config file
     mkdir -p /goli/config
@@ -91,7 +115,11 @@ function install() {
     exit 0
 }
 
-function remove() {
+update() {
+    compile_and_install_binaries
+}
+
+remove() {
     echo "Removing Goli ..."
 
     # Stop the service first
@@ -106,7 +134,6 @@ function remove() {
 
     exit 0
 }
-
 
 source_func
 
